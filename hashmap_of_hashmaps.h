97be1af690cf0fc9dc287329dd06291cb42f1d38
Theron <t.jundipinheirob@ufl.edu>
2025-10-28 18:37:28 -0400

    just the bigger hash map

--- hashmap_of_hashmaps.h
+++ hashmap_of_hashmaps.h
@@ -2,6 +2,7 @@
 #define PROJECT2_HASHMAP_OF_HASHMAPS_H
 
 #include <iostream>
+#include <unordered_map>
 
 int hashFunction(std::string name, int size);{
 int key_size = 0;
@@ -13,7 +14,7 @@ return key_size % size;
 
 class hashmap_of_hashmaps {
 
-
+std::unordered_map<std::string, std::unordered_map> filter; //the bigger has maps that the keys are categories like, SAT scores, Tuition price, school size.
 
 public:
 
