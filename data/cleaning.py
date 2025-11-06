ee804ee36ce13a20000fd1fcc59a61cd9be92dff
mariagnovais <mgalvanidenovais@ufl.edu>
2025-11-06 11:09:08 -0500

    flight light work

--- data/cleaning.py
+++ data/cleaning.py
@@ -2,97 +2,73 @@ import pandas as pd
 
 # ISSUES TO WORK ON FLIGHT:
 # ----------------------------------------------------------------------------------------------------------
-# IN MAJORS.CSV TAKE OUT BACHELOR'S DEGREE COLUMN
-# IN ONLY MAJORSNAME.CSV, PUT THE LEADING ZEROS FROM CIPCODE
+# DONE, NO FILE. IN MAJORS.CSV TAKE OUT BACHELOR'S DEGREE COLUMN
+# DONE TESTED. IN ONLY MAJORSNAME.CSV, PUT THE LEADING ZEROS FROM CIPCODE
 # CREATE A SCRIPT TO ADD MAJORS TO COLLEGES THAT DONT HAVE ONE
 # RE-SEE NORMALIZE NAMES IN MERGING.PY
 # START FRONT END
 
-
 # NAMES CLEANING
-# csv_path = 'data\college_data_cleaned_100k.csv'
+def names_cleaning():
+   csv_path = r"data\college_data_cleaned_100k.csv"
 
-# # Read the CSV file
-# data = pd.read_csv(csv_path)
+   # Read the CSV file
+   data = pd.read_csv(csv_path)
 
-# #Remove duplicates
-# data = data.drop_duplicates(subset=['college_name'], keep='first')
+   #Remove duplicates
+   data = data.drop_duplicates(subset=['college_name'], keep='first')
 
 
-# # Save only the institution names to a new CSV file 
-# newdata = data
-# output_csv_path = 'data\college_data.csv'
-# newdata.to_csv(output_csv_path, index=False)
+   # Save only the institution names to a new CSV file 
+   newdata = data
+   output_csv_path = r"data\college_data.csv"
+   newdata.to_csv(output_csv_path, index=False)
 
 
 #MAJORS
-# # Cleaning up only bachelor's majors, their cip codes, and institutions
-# csv_path = r"data\Most-Recent-Cohorts-Field-of-Study.csv"
+def majors_file():
+   # Cleaning up only bachelor's majors, their cip codes, and institutions
+   csv_path = r"data\Most-Recent-Cohorts-Field-of-Study.csv"
 
-# # Read the CSV file
-# data = pd.read_csv(csv_path, usecols=['INSTNM','CIPCODE', 'CIPDESC', 'CREDDESC'], dtype={'CIPCODE': str})
+   # Read the CSV file
+   data = pd.read_csv(csv_path, usecols=['INSTNM','CIPCODE', 'CIPDESC', 'CREDDESC'], dtype={'CIPCODE': str})
 
-# filtered_bachelors = data.loc[data['CREDDESC'] == "Bachelor's Degree"]
+   filtered_bachelors = data.loc[data['CREDDESC'] == "Bachelor's Degree"]
+   filtered_bachelors = filtered_bachelors.drop(columns=['CREDDESC'])
 
-# # print(filtered_bachelors.head())
-# # Save only the filtered to a new CSV file 
-# output_csv_path = r"data\majors.csv"
-# filtered_bachelors.to_csv(output_csv_path, index=False)
-
-# NAMES 2
-# csv_path = r"data\majors.csv"
-
-# # Read the CSV file
-# inst_name = pd.read_csv(csv_path, usecols=['INSTNM'])
-
-# #Remove duplicates
-# inst_name = inst_name.drop_duplicates()
-
-# # Save only the institution names to a new CSV file 
-# names = inst_name
-# output_csv_path = r"data\only_namesmajorfile.csv"
-# names.to_csv(output_csv_path, index=False)
-
-# Compare differences in institution names between two files
-# csv_path1 = r"data\only_names.csv"
-# csv_path2 = r"data\only_namesmajorfile.csv"
-
-# # Read the CSV files
-# names1 = pd.read_csv(csv_path1)
-# names2 = pd.read_csv(csv_path2)
-
-# Find names in names2 that are not in names1
-# differences = names1[~names1['college_name'].isin(names2['INSTNM'])] 
-
-# print(differences)
-
-# len
-# len1 = len(names1)
-# len2 = len(names2)
-# print(f"Number of unique institution names in only_names.csv: {len1}")
-# print(f"Number of unique institution names in only_namesmajorfile.csv: {len2}")
-
-# # Save majors
-csv_path = r"data\majors.csv"
-
-# # Read the CSV file
-majors = pd.read_csv(csv_path, usecols=['CIPCODE', 'CIPDESC'])
-
-#Remove duplicates
-majors = majors.drop_duplicates()
-
-# Remove ' "" ' and "." from the major names
-def nopoints_names(s : pd.Series) -> pd.Series:
-   s = s.astype(str)
-   s = s.str.replace('"', '', regex= False)
-   s = s.str.replace(r"\.", "", regex=True)
-   s =  s.str.replace(r"\s+", " ", regex=True)
-   return s
+   # print(filtered_bachelors.head())
+   # Save only the filtered to a new CSV file 
+   output_csv_path = r"data\majors.csv"
+   filtered_bachelors.to_csv(output_csv_path, index=False)
+   return
 
 
-majors['CIPDESC'] = nopoints_names(majors['CIPDESC'])
+# Save majors
+def save_majors():
+   csv_path = r"data\majors.csv"
 
-print(majors.head())
-# Save only the filtered to a new CSV file
-output_csv_path = r"data\only_majorsname.csv"
-majors.to_csv(output_csv_path, index=False)
+   # Read the CSV file
+   majors = pd.read_csv(csv_path, usecols=['CIPCODE', 'CIPDESC'],  dtype={'CIPCODE': str})
+
+   #Remove duplicates
+   majors = majors.drop_duplicates()
+
+   # Remove ' "" ' and "." from the major names
+   def nopoints_names(s : pd.Series) -> pd.Series:
+      s = s.astype(str)
+      s = s.str.replace('"', '', regex= False)
+      s = s.str.replace(r"\.", "", regex=True)
+      s =  s.str.replace(r"\s+", " ", regex=True)
+      return s
+
+
+   majors['CIPDESC'] = nopoints_names(majors['CIPDESC'])
+
+   print(majors.head())
+   # Save only the filtered to a new CSV file
+   output_csv_path = r"data\only_majorsname.csv"
+   majors.to_csv(output_csv_path, index=False)
+   return
+
+
+save_majors()
--- src/interface/UserInterface.h
+++ src/interface/UserInterface.h
@@ -3,3 +3,25 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
+class Button
+{
+    sf::Sprite sprite;
+    sf::Vector2f position;
+    std::function<void(void)> buttonClicked;
+    public:
+    Button(sf::Vector2f _position, std::function<void(void)> _onClick);
+    sf::Vector2f getPosition(); // returns position of button
+    sf::Sprite *getSprite(); // returns sprite of button
+    void setSprite(sf::Sprite _sprite); // sets sprite
+    void onClick() { buttonClicked(); }; // invokes callback method when button clicked
+}
+
+class window
+{
+public:
+    sf::RenderWindow window;
+    // user clicks on start
+    Button *starting;
+    // user clicks on exit
+    Button *exit;
+};
